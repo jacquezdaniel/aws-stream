c04d3e87e18f85c59f52e15fd6108bca6f5080a6
Danny <jacquez.j.daniel@gmail.com>
2020-04-06 17:50:59 -0700

    css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "../node_modules/materialize-css/dist/css/materialize.min.css";
 import "../node_modules/materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 import "video.js/dist/video-js.css";
+import { Parallax } from "react-materialize";
 
 class VideoPlayer extends React.Component {
   componentDidMount() {
@@ -53,17 +54,25 @@ function App() {
   return (
     <>
       <nav>
-        <div className="nav-wrapper white">
-          <a href="/" id="nav" className="brand-logo black-text">
-            High Fever Live
+        <div className="nav-wrapper black">
+          <a href="/" id="nav" className="brand-logo center" alt="High Fever">
+            <img src="fever.png" width="100" height="64" alt="High Fever"></img>
           </a>
         </div>
       </nav>
-      <div id="vContainer" className="container">
-        <VideoPlayer id="vPlayer" {...videoJsOptions} />
+      <Parallax
+        id="parallax"
+        image={<img alt="" src="corona.jpg" />}
+        options={{
+          responsiveThreshold: 0,
+        }}
+      />
+      <div className="section orange">
+        <h5 className="center white-text">Chat Room (Coming Soon)</h5>
+        <br />
       </div>
-      <div className="container">
-        <h5 className="center">Chat Room (Coming Soon)</h5>
+      <div id="vSection" className="section black">
+        <VideoPlayer id="vPlayer" {...videoJsOptions} />
       </div>
     </>
   );
